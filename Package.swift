3432ec307e718a89f6c7c2aef200af49b1ae9587
k.perminov <lestar348@gmail.com>
2024-08-30 09:13:55 +0300

    fix formating

--- Package.swift
+++ Package.swift
@@ -3,30 +3,31 @@
 import PackageDescription
 
 let package = Package(
-  name: "Tun2SocksKit",
-  platforms: [.iOS(.v13), .macCatalyst(.v13)],
-  products: [
-    .library(
-        name: "Tun2SocksKit",
-        targets: ["Tun2SocksKit"]
-    ),
-    .library(
-        name: "Tun2SocksKitC",
-        targets: ["Tun2SocksKitC"]
-    )
-  ],
-  targets: [
-    .target(
-        name: "Tun2SocksKit",
-        dependencies: ["HevSocks5Tunnel", "Tun2SocksKitC"]
-    ),
-    .target(
-        name: "Tun2SocksKitC",
-        publicHeadersPath: "."
-    ),
-    .binaryTarget(
-        name: "HevSocks5Tunnel",
-        path: "HevSocks5Tunnel.xcframework"
-    )
-  ]
+    name: "Tun2SocksKit",
+    platforms: [.iOS(.v13),
+                .macCatalyst(.v13)],
+    products: [
+        .library(
+            name: "Tun2SocksKit",
+            targets: ["Tun2SocksKit"]
+        ),
+        .library(
+            name: "Tun2SocksKitC",
+            targets: ["Tun2SocksKitC"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Tun2SocksKit",
+            dependencies: ["HevSocks5Tunnel", "Tun2SocksKitC"]
+        ),
+        .target(
+            name: "Tun2SocksKitC",
+            publicHeadersPath: "."
+        ),
+        .binaryTarget(
+            name: "HevSocks5Tunnel",
+            path: "HevSocks5Tunnel.xcframework"
+        )
+    ]
 )
